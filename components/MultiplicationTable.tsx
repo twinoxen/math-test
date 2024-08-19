29b41f2c476ddf6b13483927542b5edcda560e4a
twinoxen <1854037+twinoxen@users.noreply.github.com>
2024-08-18 22:05:23 -0400

    update styling

---
+++ components/MultiplicationTable.tsx
@@ -0,0 +1,185 @@
+import React, { useEffect, useState } from 'react';
+
+const MultiplicationTable: React.FC = () => {
+  const [multiplicationNumbersRange, setMultiplicationNumbersRange] = useState(
+    new Array(11).fill('').map((a, i) => i)
+  );
+  const [multiplicationMin, setMultiplicationMin] = useState(0);
+  const [multiplicationMax, setMultiplicationMax] = useState(10);
+
+  const onlyNumber = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.value === '' && !/^[0-9\b]+$/.test(event.target.value)) {
+      event.target.style.backgroundColor = 'white';
+
+      event.target.value = event.target.value.replace(/[a-zA-Z;:\/\?]/gm, '');
+      return false;
+    }
+
+    return true;
+  };
+
+  const focusNextInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const input = event.target;
+
+    if (!input) return;
+
+    const next = input.parentNode?.nextSibling;
+
+    if (!next) return;
+
+    const childInput = next.firstChild as HTMLInputElement;
+
+    childInput?.focus();
+  };
+
+  const setMultiRange = () => {
+    if (
+      multiplicationMin >= multiplicationMax ||
+      isNaN(multiplicationMin) ||
+      isNaN(multiplicationMax)
+    )
+      return;
+
+    const range: number[] = [];
+
+    for (
+      let index = multiplicationMin;
+      index < multiplicationMax + 1;
+      index++
+    ) {
+      range.push(index);
+    }
+
+    setMultiplicationNumbersRange(range);
+  };
+
+  const setMultiMin = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!onlyNumber(event)) {
+      return;
+    }
+
+    const input = parseInt(event.target.value);
+
+    if (input < 0) return;
+
+    setMultiplicationMin(input);
+  };
+
+  const setMultiMax = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!onlyNumber(event)) {
+      return;
+    }
+
+    const input = parseInt(event.target.value);
+
+    if (input < 0) return;
+
+    setMultiplicationMax(input);
+  };
+
+  useEffect(setMultiRange, [multiplicationMin, multiplicationMax]);
+
+  const validateMultiplicationInput = (
+    value: number,
+    column: number,
+    row: number
+  ) => {
+    return value === column * row;
+  };
+
+  const handleMultiplicationInput =
+    (column: number, row: number) =>
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      if (!onlyNumber(event)) {
+        return;
+      }
+
+      const input = parseInt(event.target.value);
+
+      if (!validateMultiplicationInput(input, column, row)) {
+        event.target.style.backgroundColor = 'red';
+
+        return;
+      }
+
+      event.target.style.backgroundColor = 'green';
+      focusNextInput(event);
+    };
+
+  return (
+    <section className="">
+      <h2 className="text-center font-bold text-2xl ">
+        Multiplication table
+      </h2>
+      <p className="print-hide text-center p-4 flex gap-4 items-center justify-center">
+        <span>Enter Range</span>
+        <input
+          className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent bg-gray-100"
+          value={multiplicationMin}
+          onChange={setMultiMin}
+          placeholder="0"
+          type="text"
+          size={4}
+          pattern="[0-9]*"
+        />
+        <span>-</span>
+        <input
+          className=" p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent bg-gray-100"
+          value={multiplicationMax}
+          onChange={setMultiMax}
+          placeholder="0"
+          type="text"
+          size={4}
+          pattern="[0-9]*"
+        />
+      </p>
+
+      <div className="flex justify-center">
+        <div className="grow-0 shrink-0 flex flex-col border-l border-t border-r border-black">
+          {multiplicationNumbersRange.map((parentItem, index) => (
+            <div
+              className="w-[40px] h-[40px] leading-10 border-b border-black text-lg text-center"
+              key={`y-axis-${parentItem}`}
+            >
+              {index}
+            </div>
+          ))}
+        </div>
+        <div className="overflow-x-auto">
+          <div className="flex border-black">
+            {multiplicationNumbersRange.slice(1).map((parentItem, index) => (
+              <div
+                className="w-[40px] h-[41px] shrink-0 leading-10 border-b border-r border-t border-black text-lg text-center"
+                key={`x-axis-${parentItem}`}
+              >
+                {index + 1}
+              </div>
+            ))}
+          </div>
+
+          {multiplicationNumbersRange.slice(1).map((parentIndex) => (
+            <div key={`row-${parentIndex}`} className="flex shrink-0">
+              {multiplicationNumbersRange.slice(1).map((childIndex, index) => (
+                <div
+                  className="w-[40px] h-[40px] shrink-0 leading-10 border-b border-r border-black text-lg text-center"
+                  key={`input-${parentIndex}-${childIndex}`}
+                >
+                  <input
+                    className="w-full h-full text-center rounded-none"
+                    onChange={handleMultiplicationInput(
+                      parentIndex,
+                      childIndex
+                    )}
+                    pattern="[0-9]*"
+                  />
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MultiplicationTable;
---
+++ components/ProblemGenerator.tsx
@@ -0,0 +1,382 @@
+import React, { createRef, useEffect, useRef, useState } from 'react';
+import Image from 'next/image';
+
+export interface Problem {
+  left: number;
+  right: number;
+  type: string;
+  passed?: boolean;
+  time?: number;
+  elapsed?: number;
+}
+
+const ProblemGenerator: React.FC = () => {
+  const [generatedProblems, setGeneratedProblems] = useState<Problem[]>([]);
+  const [problemType, setProblemType] = useState([
+    { name: 'Addition', symbol: '+', checked: true },
+    { name: 'Subtraction', symbol: '-', checked: false },
+    { name: 'Multiplication', symbol: '*', checked: false },
+    { name: 'Division', symbol: '/', checked: false },
+  ]);
+  const [numberOfProblems, setNumberOfProblems] = useState(10);
+  const [problemMin, setProblemMin] = useState(1);
+  const [problemMax, setProblemMax] = useState(10);
+
+  const problemRefs = useRef([]);
+
+  const [time, setTime] = useState(0);
+  const [lastTime, setLastTime] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const [isPaused, setIsPaused] = useState(true);
+
+  const onlyNumber = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.value === '' && !/^[0-9\b]+$/.test(event.target.value)) {
+      event.target.style.backgroundColor = 'white';
+
+      event.target.value = event.target.value.replace(/[a-zA-Z;:\/\?]/gm, '');
+      return false;
+    }
+
+    return true;
+  };
+
+  const randomNumber = (min: number, max: number) => {
+    return Math.floor(Math.random() * (max - min) + min);
+  };
+
+  const readableType = (type: string) => {
+    const map: { [key: string]: string } = {
+      '+': '+',
+      '-': '-',
+      '*': 'x',
+      '/': '÷',
+    };
+
+    return map[type];
+  };
+
+  const focusNextInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const input = event.target;
+
+    if (!input) return;
+
+    const next = input.parentNode?.nextSibling;
+
+    if (!next) return;
+
+    const childInput = next.firstChild as HTMLInputElement;
+
+    childInput?.focus();
+  };
+
+  const generateProblems = () => {
+    if (problemMin >= problemMax || isNaN(problemMin) || isNaN(problemMax))
+      return;
+
+    handleReset();
+    handleStart();
+
+    const problems: Problem[] = [];
+
+    const selectedProblemTypes = problemType.filter((type) => type.checked);
+
+    for (let index = 0; index < numberOfProblems; index++) {
+      const left = randomNumber(problemMin, problemMax);
+      const right = randomNumber(problemMin, problemMax);
+
+      problems.push({
+        left,
+        right,
+        type: selectedProblemTypes[
+          Math.floor(Math.random() * selectedProblemTypes.length)
+        ].symbol,
+      });
+    }
+
+    if (problemRefs.current.length !== problems.length) {
+      problemRefs.current = Array(problems.length)
+        .fill('')
+        .map((_, i) => problemRefs.current[i] || createRef());
+    }
+
+    setGeneratedProblems(problems);
+  };
+
+  const evaluate = (input: number, problem: Problem) => {
+    let result: number | null = null;
+
+    switch (problem.type) {
+      case '+':
+        result = problem.left + problem.right;
+        break;
+      case '-':
+        result = problem.left - problem.right;
+        break;
+      case '*':
+        result = problem.left * problem.right;
+        break;
+      case '/':
+        result = problem.left / problem.right;
+        break;
+    }
+
+    return input.toFixed(2) === result?.toFixed(2);
+  };
+
+  const handleProblemType = (index: number) => () => {
+    const update = [...problemType];
+    update[index].checked = !update[index].checked;
+
+    setProblemType(update);
+  };
+
+  const handleProblemInput =
+    (problem: Problem) => (event: React.ChangeEvent<HTMLInputElement>) => {
+      if (!onlyNumber(event)) {
+        return;
+      }
+
+      if (isPaused) handlePauseResume();
+
+      const input = parseFloat(event.target.value);
+
+      if (!evaluate(input, problem)) {
+        event.target.style.backgroundColor = 'red';
+        event.target.style.color = 'white';
+
+        return;
+      }
+
+      problem.passed = true;
+      problem.time = time;
+      problem.elapsed = lastTime > 0 ? time - lastTime : time;
+
+      setLastTime(time);
+
+      event.target.style.backgroundColor = 'green';
+      event.target.style.color = 'white';
+
+      focusNextInput(event);
+
+      if (
+        generatedProblems.length &&
+        generatedProblems.every((problem) => problem.passed === true) &&
+        !isPaused
+      ) {
+        handlePauseResume();
+      }
+    };
+
+  const handleStart = () => {
+    setIsActive(true);
+    setIsPaused(false);
+  };
+
+  const handlePauseResume = () => {
+    setIsPaused(!isPaused);
+  };
+
+  const handleReset = () => {
+    setIsActive(false);
+    setTime(0);
+  };
+
+  useEffect(() => {
+    problemRefs.current.forEach((item: any) => {
+      if (!item.current) return;
+
+      item.current.value = '';
+      item.current.style.backgroundColor = '#fff';
+      item.current.style.color = '#000';
+    });
+  }, [generatedProblems]);
+
+  useEffect(() => {
+    let interval: NodeJS.Timer | null = null;
+
+    if (isActive && isPaused === false) {
+      interval = setInterval(() => {
+        setTime((time) => time + 10);
+      }, 10);
+    } else {
+      if (interval) clearInterval(interval);
+    }
+    return () => {
+      if (interval) clearInterval(interval);
+    };
+  }, [isActive, isPaused]);
+
+  return (
+    <section className="flex flex-col justify-center items-center">
+      <div className="max-w-md mb-8">
+        <h2 className="print-hide mt-10 font-bold text-2xl text-center mb-4">
+          Generate random math problems
+        </h2>
+
+        <div className="print-hide pl-4 pr-4 mb-4">
+          <div className="font-bold mb-4">Type of problem</div>
+          <div className="grid grid-cols-2 gap-4 mb-8">
+            {problemType.map((type, index) => {
+              return (
+                <div
+                  key={index}
+                  onClick={handleProblemType(index)}
+                  className="flex items-center gap-2"
+                >
+                  <input
+                    name="problemType"
+                    type="checkbox"
+                    checked={type.checked}
+                    value={type.symbol}
+                    onChange={handleProblemType(index)}
+                    pattern="[0-9]*"
+                    className=""
+                  />
+                  <label className="ml-1" htmlFor="problemType">
+                    {type.name}
+                  </label>
+                </div>
+              );
+            })}
+          </div>
+
+          <div className="grid grid-cols-2 items-center gap-4 mb-8">
+            <div className="font-bold">Number of problems</div>
+            <input
+              className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent bg-gray-100"
+              value={numberOfProblems}
+              onChange={(event) =>
+                setNumberOfProblems(parseInt(event.target.value))
+              }
+              placeholder="0"
+              type="text"
+              size={4}
+              pattern="[0-9]*"
+            />
+
+            <div className="font-bold">Number range </div>
+            <div className="flex gap-4 items-center">
+              <input
+                className="grow p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent bg-gray-100"
+                value={problemMin}
+                onChange={(event) =>
+                  setProblemMin(parseInt(event.target.value))
+                }
+                placeholder="0"
+                type="text"
+                size={4}
+                pattern="[0-9]*"
+              />
+              <span>-</span>
+              <input
+                className=" grow p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent bg-gray-100"
+                value={problemMax}
+                onChange={(event) =>
+                  setProblemMax(parseInt(event.target.value))
+                }
+                placeholder="0"
+                type="text"
+                size={4}
+                pattern="[0-9]*"
+              />
+            </div>
+          </div>
+
+          <div className="flex items-center">
+            <div className="mb-4 grow " onClick={handlePauseResume}>
+              <div className="flex gap-1 items-center mt-4">
+                <div className="h-[1.875rem]">
+                  <Image
+                    src="/timer-icon.svg"
+                    alt="timer"
+                    width={30}
+                    height={30}
+                    style={{ cursor: 'pointer' }}
+                  />
+                </div>
+                <Time time={time} />
+              </div>
+            </div>
+            <div className="">
+              <button
+                className="bg-emerald-500 hover:bg-emerald-700 text-white font-bold py-2 px-4 rounded"
+                onClick={generateProblems}
+              >
+                Generate
+              </button>
+            </div>
+          </div>
+        </div>
+
+        {problemType.find(
+          (type) => type.name === 'Division' && type.checked
+        ) && (
+          <p className='text-sm mb-8'>
+            *precision to the nearest hundredth decimal exp: 1.03
+          </p>
+        )}
+
+        <div className="grid grid-cols-2 gap-4 items-center">
+          {generatedProblems.map((problem, index) => {
+            return (
+              <div
+                key={`problem-${index}`}
+                className="flex items-center gap-4"
+                style={{ position: 'relative' }}
+              >
+                <span className='grow text-right'>{`${problem.left} ${readableType(problem.type)} ${
+                  problem.right
+                } = `}</span>
+                <input
+                  className="grow-0 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent bg-gray-400"
+                  size={5}
+                  ref={problemRefs.current[index]}
+                  onChange={handleProblemInput(problem)}
+                  pattern="[0-9]*"
+                />
+                {problem.elapsed && (
+                  <div
+                    style={{
+                      position: 'absolute',
+                      display: 'flex',
+                      gap: '0.15rem',
+                      right: '0.4rem',
+                      color: '#99D492',
+                    }}
+                  >
+                    <Time time={problem.elapsed} />
+                    <Image
+                      src="/timer-icon.svg"
+                      alt="timer"
+                      width={15}
+                      height={15}
+                      style={{ cursor: 'pointer' }}
+                    />
+                  </div>
+                )}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ProblemGenerator;
+
+const Time = ({ time }: { time: number }) => {
+  return (
+    <div className="timer">
+      <span className="digits">
+        {('0' + Math.floor((time / 60000) % 60)).slice(-2)}:
+      </span>
+      <span className="digits">
+        {('0' + Math.floor((time / 1000) % 60)).slice(-2)}.
+      </span>
+      <span className="digits mili-sec">
+        {('0' + ((time / 10) % 100)).slice(-2)}
+      </span>
+    </div>
+  );
+};
